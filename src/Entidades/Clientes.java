5a5ac81b18677a402ee60e53dea3acc330b777c9
Henrique Parucker <Henrique Parucker@Henrique-PC-G1511V2>
2016-06-23 23:33:30 -0300

    Criação de classes

--- src/Entidades/Clientes.java
+++ src/Entidades/Clientes.java
@@ -5,27 +5,27 @@
  */
 package Entidades;
 
+
 import java.util.Date;
+
 
     
     
 public class Clientes {
     
-    String nome;
-    int idPessoa;
-    String endereco;
-    int cpf;
-    int cnpj;
-    Date nascimento;
-    String sexo;
-    String razaoSocial;
-    String tel1, tel2;
+    private String nome;
+    private static int idPessoa;
+    private String endereco;
+    private String documento;
+    private final static byte CPF = 0;
+    private final static byte CNPJ = 1;
+    private Date nascimento;
+    private String sexo;
+    private String razaoSocial;
+    private String tel1, tel2;
     
-    public void busca(String a){}        
-    }
-    public void excluirCliente(String a){
-    }
-    public void alterarCliente{}
-    
- 
+   
+        
+        
+          
 }
---
+++ src/Entidades/DescricaoVoo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author Henrique Parucker
+ */
+public class DescricaoVoo{
+    private long idDescricaoVoo;
+    private String horarioPartida;
+    private String horarioChegada;
+    private long idAeroportoOrigem;
+    private long idAeroportoChegada;
+    
+}
