85c2d46d720d1c3881184db6f1045d5bc3d4891b
Henrique Parucker <Henrique Parucker@Henrique-PC-G1511V2>
2016-06-23 22:25:44 -0300

    Foi adicionado classe clientes incompleta

---
+++ src/Entidades/Clientes.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+import java.util.Date;
+
+    
+    
+public class Clientes {
+    
+    String nome;
+    int idPessoa;
+    String endereco;
+    int cpf;
+    int cnpj;
+    Date nascimento;
+    String sexo;
+    String razaoSocial;
+    String tel1, tel2;
+    
+    public void busca(String a){}        
+    }
+    public void excluirCliente(String a){
+    }
+    public void alterarCliente{}
+    
+ 
+}
